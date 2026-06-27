65ee4c0d4ea93080bff785d8148e5f4f62733b8b
Adiba Shahab <adibashahab9@gmail.com>
2026-06-27 11:25:39 +0530

    Fix pyramid pattern logic in Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,12 +1,12 @@
 public class Pyramid{
   public static void main(String[]args){
     int n = 5;
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n - i - 1; j++){
+    for(int i = 1; i <= n; i++){
+      for(int j = 0; j <= n - i; j++){
         System.out.print(" ");
       }
-      for(int j = 0; j <= i; j++){
-        System.out.print("* ");
+      for(int j = 1; j <= (2*i-1); j++){
+        System.out.print("*");
       }
       System.out.println();
     }
