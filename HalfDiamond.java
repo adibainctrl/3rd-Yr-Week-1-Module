d07853ef270829f9f620de85c1d9528ccccc254a
Adiba Shahab <adibashahab9@gmail.com>
2026-06-28 08:18:00 +0530

    Fix syntax error in HalfDiamond.java

--- HalfDiamond.java
+++ HalfDiamond.java
@@ -5,7 +5,7 @@ public class HalfDiamond{
       for(int j=1; j<=i ; j++){
         System.out.print("*");
       }
-      System.out.println():
+      System.out.println();
         }
     for(int i=n-1; i>=1 ; i--){
       for(int j=1; j<=i ; j++){
