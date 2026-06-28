c8b60e0da7f8e9308b07e59f3c752ccc94f51ec1
Adiba Shahab <adibashahab9@gmail.com>
2026-06-28 08:11:08 +0530

    Refactor Diamond class to print diamond shape

--- Diamond.java
+++ Diamond.java
@@ -1,21 +1,21 @@
 public class Diamond{
   public static void main(String[] args){
     int n = 5;
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j<n-i-1 ; j++){
+    for(int i = 1; i <= n; i++){
+      for(int s = 1; s<=n-i ; s++){
         System.out.print(" ");
       }
-      for(int j = 0; j <= i; j++){
-        System.out.print("* ");
+      for(int j = 1; j <= (2*i-1); j++){
+        System.out.print("*");
       }
       System.out.println();
     }
-    for(int i = 1; i < n; i++){
-      for(int j = 0; j < i; j++){
+    for(int i = n-1; i >= 1; i--){
+      for(int s = 1; s <= n-i; s++){
         System.out.print(" ");
       }
-      for(int j = 0; j < n-i; j++){
-        System.out.print("* ");
+      for(int j = 1; j < (2*i-1); j++){
+        System.out.print("*");
           }
       System.out.println();
     }
